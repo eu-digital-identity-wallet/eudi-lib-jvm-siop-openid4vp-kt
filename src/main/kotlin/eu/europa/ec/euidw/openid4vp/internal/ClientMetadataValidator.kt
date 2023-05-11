d02ff1775019706e17d2ee6d19b055a4bfa02d3e
Routis Babis <haralampos.routis@netcompany-intrasoft.com>
2023-05-11 10:23:25 +0300

    Minor code cleanup

--- src/main/kotlin/eu/europa/ec/euidw/openid4vp/internal/ClientMetadataValidator.kt
+++ src/main/kotlin/eu/europa/ec/euidw/openid4vp/internal/ClientMetadataValidator.kt
@@ -6,41 +6,44 @@ import com.nimbusds.jose.JWSAlgorithm
 import com.nimbusds.jose.jwk.JWKSet
 import com.nimbusds.openid.connect.sdk.rp.OIDCClientMetadata
 import eu.europa.ec.euidw.openid4vp.ClientMetaData
+import kotlinx.coroutines.Dispatchers
 import java.net.URL
 
 object ClientMetadataValidator {
 
-    fun validate(clientMetadata : ClientMetaData) : Result<OIDCClientMetadata> {
+    suspend fun  validate(clientMetadata: ClientMetaData): Result<OIDCClientMetadata> {
         val isValid = hasRequiredJwksSource(clientMetadata) && hasSubjectSyntaxTypes(clientMetadata)
         return when {
-            isValid ->  Result.success(valid(clientMetadata))
+            isValid -> Result.success(valid(clientMetadata))
             else -> Result.failure(IllegalStateException("Client metadata wrong syntax"))
         }
     }
 
-    private fun hasRequiredJwksSource(clientMetadata: ClientMetaData) : Boolean {
+    private fun hasRequiredJwksSource(clientMetadata: ClientMetaData): Boolean {
         val bothJwksSourcesDefined = !clientMetadata.jwks.isNullOrEmpty() && !clientMetadata.jwksUri.isNullOrEmpty()
         val atLeastOneJwkSourceDefined = !clientMetadata.jwks.isNullOrEmpty() || !clientMetadata.jwksUri.isNullOrEmpty()
         return !bothJwksSourcesDefined && atLeastOneJwkSourceDefined
     }
 
-    private fun hasSubjectSyntaxTypes(clientMetadata: ClientMetaData) : Boolean {
-        return !clientMetadata.subjectSyntaxTypesSupported.isEmpty()
-    }
+    private fun hasSubjectSyntaxTypes(clientMetadata: ClientMetaData) =
+        clientMetadata.subjectSyntaxTypesSupported.isNotEmpty()
 
-    private fun valid(cmtd: ClientMetaData): OIDCClientMetadata {
-        val vJwkSet: JWKSet
-        if (!cmtd.jwksUri.isNullOrEmpty()) {
-            vJwkSet = JWKSet.load(URL(cmtd.jwksUri))
-        } else {
-            vJwkSet = JWKSet.parse(cmtd.jwks?.toString())
+    private suspend fun valid(clientMetaData: ClientMetaData): OIDCClientMetadata {
+
+        val vJwkSet = when {
+            clientMetaData.jwksUri.isNullOrEmpty() -> JWKSet.parse(clientMetaData.jwks?.toString())
+            else -> {
+                // TODO this should be launched in coroutine, since it is blocking
+                JWKSet.load(URL(clientMetaData.jwksUri))
+            }
         }
+
         return OIDCClientMetadata().apply {
-            idTokenJWSAlg = JWSAlgorithm.parse(cmtd.idTokenSignedResponseAlg)
-            idTokenJWEAlg = JWEAlgorithm.parse(cmtd.idTokenEncryptedResponseAlg)
-            idTokenJWEEnc = EncryptionMethod.parse(cmtd.idTokenEncryptedResponseEnc)
+            idTokenJWSAlg = JWSAlgorithm.parse(clientMetaData.idTokenSignedResponseAlg)
+            idTokenJWEAlg = JWEAlgorithm.parse(clientMetaData.idTokenEncryptedResponseAlg)
+            idTokenJWEEnc = EncryptionMethod.parse(clientMetaData.idTokenEncryptedResponseEnc)
             jwkSet = vJwkSet
-            setCustomField("subject_syntax_types_supported", cmtd.subjectSyntaxTypesSupported)
+            setCustomField("subject_syntax_types_supported", clientMetaData.subjectSyntaxTypesSupported)
         }
     }
 }

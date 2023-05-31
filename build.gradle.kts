f6a27c0203ab5eb6cbdcde4585d9ffa534d2dc70
Babis Routis <127745316+babisRoutis@users.noreply.github.com>
2023-05-31 14:39:25 +0300

    Added license header file to source code (#21)

--- build.gradle.kts
+++ build.gradle.kts
@@ -87,6 +87,7 @@ val ktlintVersion = "0.49.1"
 spotless {
     kotlin {
         ktlint(ktlintVersion)
+        licenseHeaderFile("LICENSE-HEADER.txt")
     }
     kotlinGradle {
         ktlint(ktlintVersion)

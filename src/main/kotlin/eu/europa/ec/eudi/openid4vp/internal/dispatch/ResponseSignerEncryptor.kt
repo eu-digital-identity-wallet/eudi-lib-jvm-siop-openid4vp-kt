2bcdce94e1c0e381363223135a8e8a2a03b65c8b
Routis Babis <haralampos.routis@netcompany-intrasoft.com>
2023-07-27 13:42:06 +0300

    Improve ResponseSignerEncryptor

--- src/main/kotlin/eu/europa/ec/eudi/openid4vp/internal/dispatch/ResponseSignerEncryptor.kt
+++ src/main/kotlin/eu/europa/ec/eudi/openid4vp/internal/dispatch/ResponseSignerEncryptor.kt
@@ -16,13 +16,13 @@
 package eu.europa.ec.eudi.openid4vp.internal.dispatch
 
 import com.nimbusds.jose.*
+import com.nimbusds.jose.JWEAlgorithm.Family
 import com.nimbusds.jose.crypto.ECDHEncrypter
-import com.nimbusds.jose.crypto.ECDSASigner
 import com.nimbusds.jose.crypto.RSAEncrypter
-import com.nimbusds.jose.crypto.RSASSASigner
+import com.nimbusds.jose.crypto.factories.DefaultJWSSignerFactory
 import com.nimbusds.jose.jwk.ECKey
+import com.nimbusds.jose.jwk.JWK
 import com.nimbusds.jose.jwk.JWKSet
-import com.nimbusds.jose.jwk.KeyType
 import com.nimbusds.jose.jwk.RSAKey
 import com.nimbusds.jwt.EncryptedJWT
 import com.nimbusds.jwt.JWTClaimsSet
@@ -35,107 +35,71 @@ internal object ResponseSignerEncryptor {
 
     fun signEncryptResponse(spec: JarmSpec, data: AuthorizationResponsePayload): String {
         return when (spec) {
-            is JarmSpec.SignedResponse -> sign(spec, data)
-            is JarmSpec.EncryptedResponse -> encrypt(spec, data)
-            is JarmSpec.SignedAndEncryptedResponse -> signAndEncrypt(spec, data)
+            is JarmSpec.SignedResponse -> sign(spec, data).serialize()
+            is JarmSpec.EncryptedResponse -> encrypt(spec, data).serialize()
+            is JarmSpec.SignedAndEncryptedResponse -> signAndEncrypt(spec, data).serialize()
         }
     }
 
-    private fun sign(spec: JarmSpec.SignedResponse, data: AuthorizationResponsePayload): String {
-        return dataAsSignedJWT(data, spec.responseSigningAlg, spec.signingKeySet, spec.holderId).serialize()
+    private fun sign(
+        spec: JarmSpec.SignedResponse,
+        data: AuthorizationResponsePayload,
+    ): SignedJWT {
+        val (holderId, signingAlg, signingKeySet) = spec
+        val (signingKey, jwsSigner) = keyAndSigner(signingAlg, signingKeySet)
+        val header = JWSHeader.Builder(signingAlg)
+            .keyID(signingKey.keyID)
+            .build()
+        val dataAsJWT = DirectPostForm.of(data).asJWT(holderId)
+        return SignedJWT(header, dataAsJWT).apply { sign(jwsSigner) }
     }
 
-    private fun encrypt(spec: JarmSpec.EncryptedResponse, data: AuthorizationResponsePayload): String {
-        val jweEncrypter = deductEncryptor(spec.responseEncryptionAlg, spec.encryptionKeySet)
+    private fun encrypt(
+        spec: JarmSpec.EncryptedResponse,
+        data: AuthorizationResponsePayload,
+    ): EncryptedJWT {
+        val (_, jweEncrypter) = keyAndEncryptor(spec.responseEncryptionAlg, spec.encryptionKeySet)
         val jweHeader = JWEHeader(spec.responseEncryptionAlg, spec.responseEncryptionEnc)
         val dataAsJWT = DirectPostForm.of(data).asJWT(spec.holderId)
-        val encryptedJWT = EncryptedJWT(jweHeader, dataAsJWT)
-        return with(encryptedJWT) {
-            encrypt(jweEncrypter)
-            serialize()
-        }
+        return EncryptedJWT(jweHeader, dataAsJWT).apply { encrypt(jweEncrypter) }
     }
 
     private fun signAndEncrypt(
         spec: JarmSpec.SignedAndEncryptedResponse,
         data: AuthorizationResponsePayload,
-    ): String {
-        val signedJwt = dataAsSignedJWT(data, spec.responseSigningAlg, spec.signingKeySet, spec.holderId)
-        val jweEncrypter = deductEncryptor(spec.responseEncryptionAlg, spec.encryptionKeySet)
-        val jweObject = JWEObject(
+    ): JWEObject {
+        val signedJwt = sign(spec.signedResponse(), data)
+        val (_, jweEncrypter) = keyAndEncryptor(spec.responseEncryptionAlg, spec.encryptionKeySet)
+        return JWEObject(
             JWEHeader(spec.responseEncryptionAlg, spec.responseEncryptionEnc),
             Payload(signedJwt),
-        )
-        return with(jweObject) {
-            encrypt(jweEncrypter)
-            serialize()
-        }
+        ).apply { encrypt(jweEncrypter) }
     }
 
-    private fun dataAsSignedJWT(
-        data: AuthorizationResponsePayload,
-        signingAlg: JWSAlgorithm,
-        signingKeySet: JWKSet,
-        holderId: String,
-    ): SignedJWT {
-        val jwsSigner = deductSigner(signingAlg, signingKeySet)
-        val header = JWSHeader.Builder(signingAlg)
-            .keyID("") // TODO: keyId
-            .build()
-        val dataAsJWT = DirectPostForm.of(data).asJWT(holderId)
-        val signedJWT = SignedJWT(header, dataAsJWT)
-        signedJWT.sign(jwsSigner)
-        return signedJWT
+    private fun JarmSpec.SignedAndEncryptedResponse.signedResponse(): JarmSpec.SignedResponse =
+        JarmSpec.SignedResponse(holderId, responseSigningAlg, signingKeySet)
+
+    private fun keyAndSigner(
+        jwsAlgorithm: JWSAlgorithm,
+        keySet: JWKSet,
+    ): Pair<JWK, JWSSigner> {
+        val signerFactory = DefaultJWSSignerFactory()
+        fun signer(key: JWK) = runCatching { signerFactory.createJWSSigner(key, jwsAlgorithm) }.getOrNull()
+        return keySet.keys.firstNotNullOfOrNull { key ->
+            signer(key)?.let { signer -> key to signer }
+        } ?: error("Cannot find appropriate signing key for ${jwsAlgorithm.name}")
     }
 
-    private fun deductSigner(
-        signingAlg: JWSAlgorithm,
-        signingKeySet: JWKSet,
-    ): JWSSigner = when {
-        JWSAlgorithm.Family.EC.contains(signingAlg) -> createECSigner(signingKeySet)
-        JWSAlgorithm.Family.RSA.contains(signingAlg) -> createRSASigner(signingKeySet)
-        else -> throw UnsupportedOperationException(
-            "Unsupported signing algorithm $signingKeySet. Currently supported signing " +
-                "algorithm families are [EC, RSA]",
-        )
-    }
-
-    private fun deductEncryptor(
-        responseEncryptionAlg: JWEAlgorithm,
-        encryptionKeySet: JWKSet,
-    ): JWEEncrypter = when {
-        JWEAlgorithm.Family.ECDH_ES.any { it.name == responseEncryptionAlg.name } -> createECDHEncrypter(encryptionKeySet)
-        JWEAlgorithm.Family.RSA.any { it.name == responseEncryptionAlg.name } -> createRSAEncrypter(encryptionKeySet)
-        else -> throw UnsupportedOperationException(
-            "Unsupported encryption algorithm $responseEncryptionAlg." +
-                " Currently supported encryption algorithm families are [ECDH_ES, RSA]",
-        )
-    }
-
-    private fun createECDHEncrypter(keySet: JWKSet): ECDHEncrypter {
-        // Look for a EC key in JWKSet
-        val ecJWK = keySet.keys.first { it.keyType.value == KeyType.EC.value }
-            ?: throw RuntimeException("No EC encryption key found in the provided key set")
-        return ECDHEncrypter(ECKey.parse(ecJWK.toJSONObject()))
-    }
-
-    private fun createECSigner(keySet: JWKSet): JWSSigner {
-        val ecJWK = keySet.keys.first { it.keyType.value == KeyType.EC.value }
-            ?: throw RuntimeException("No EC signing key found in the provided key set")
-        return ECDSASigner(ECKey.parse(ecJWK.toJSONObject()))
-    }
-
-    private fun createRSAEncrypter(keySet: JWKSet): RSAEncrypter {
-        val rsaJWK = keySet.keys.first { it.keyType.value == KeyType.RSA.value }
-            ?: throw RuntimeException("No RSA encryption key found in the provided key set")
-        return RSAEncrypter(RSAKey.parse(rsaJWK.toJSONObject()))
-    }
-
-    private fun createRSASigner(keySet: JWKSet): JWSSigner {
-        val rsaJWK = keySet.keys.first { it.keyType.value == KeyType.RSA.value }
-            ?: throw RuntimeException("No RSA signing key found in the provided key set")
-        return RSASSASigner(RSAKey.parse(rsaJWK.toJSONObject()))
-    }
+    private fun keyAndEncryptor(
+        jweAlgorithm: JWEAlgorithm,
+        jwkSet: JWKSet,
+    ): Pair<JWK, JWEEncrypter> =
+        EncrypterFactory
+            .findEncrypters(jweAlgorithm, jwkSet)
+            .entries
+            .firstOrNull()
+            ?.toPair()
+            ?: error("Cannot find appropriate encryption key for ${jweAlgorithm.name}")
 
     private fun Map<String, String>.asJWT(holderId: String): JWTClaimsSet {
         return with(JWTClaimsSet.Builder()) {
@@ -145,4 +109,36 @@ internal object ResponseSignerEncryptor {
             build()
         }
     }
+}
+
+private object EncrypterFactory {
+
+    fun findEncrypters(
+        algorithm: JWEAlgorithm,
+        keySet: JWKSet,
+    ): Map<JWK, JWEEncrypter> {
+        fun encrypter(key: JWK) = runCatching {
+            createEncrypter(key, algorithm)
+        }.getOrNull()
+
+        return keySet.keys.mapNotNull { key ->
+            encrypter(key)?.let { encrypter -> key to encrypter }
+        }.toMap()
+    }
+
+    fun createEncrypter(
+        key: JWK,
+        algorithm: JWEAlgorithm,
+    ): JWEEncrypter? =
+        familyOf(algorithm)?.let { family ->
+            when {
+                family == Family.ECDH_ES && key is ECKey -> ECDHEncrypter(key)
+                family == Family.RSA && key is RSAKey -> RSAEncrypter(key)
+                else -> null
+            }
+        }
+
+    private val SupportedFamilies = listOf(Family.ECDH_ES, Family.RSA)
+    private fun familyOf(algorithm: JWEAlgorithm): Family? =
+        SupportedFamilies.firstOrNull { family -> family.contains(algorithm) }
 }

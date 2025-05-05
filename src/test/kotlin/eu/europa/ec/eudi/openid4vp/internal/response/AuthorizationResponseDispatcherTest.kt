ca6b53618d32c766431ddefc11e797b9ece00ee1
babisRoutis <haralampos.routis@netcompany.com>
2025-05-05 17:28:01 +0300

    Support verifier_attestations

--- src/test/kotlin/eu/europa/ec/eudi/openid4vp/internal/response/AuthorizationResponseDispatcherTest.kt
+++ src/test/kotlin/eu/europa/ec/eudi/openid4vp/internal/response/AuthorizationResponseDispatcherTest.kt
@@ -195,6 +195,7 @@ class AuthorizationResponseDispatcherTest {
                     state = state,
                     presentationQuery = PresentationQuery.ByPresentationDefinition(presentationDefinition),
                     transactionData = null,
+                    verifierAttestations = null
                 )
 
             val vpTokenConsensus = Consensus.PositiveConsensus.VPTokenConsensus(

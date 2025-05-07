f1f23120a8f11a76947ad120639500fff6fdd736
Anastasios Tsantoulis <Anastasios.TSANTOULIS@netcompany.com>
2025-05-07 17:40:53 +0300

    Let fun access engine since most of the test needed it
    Signed-off-by: Anastasios Tsantoulis <Anastasios.TSANTOULIS@netcompany.com>

--- src/test/kotlin/eu/europa/ec/eudi/openid4vp/Ktor.kt
+++ src/test/kotlin/eu/europa/ec/eudi/openid4vp/Ktor.kt
@@ -1,16 +1,15 @@
 package eu.europa.ec.eudi.openid4vp
 
 import io.ktor.client.*
+import io.ktor.client.engine.*
 import io.ktor.client.plugins.logging.*
 
 object LoggingHttpClientFactory {
-    fun createLoggingHttpClient(logging:Boolean = false,loggerImpl: Logger = Logger.SIMPLE, loggerLevel: LogLevel = LogLevel.HEADERS): HttpClient =
-        HttpClient {
-            if(logging) {
-                install(Logging){
-                    logger = loggerImpl
-                    level = loggerLevel
-                }
-            }else {}
+    fun createLoggingHttpClient(loggerImpl: Logger = Logger.SIMPLE, loggerLevel: LogLevel = LogLevel.HEADERS, httpEngine: HttpClientEngine): HttpClient =
+        HttpClient(httpEngine) {
+            install(Logging){
+                logger = loggerImpl
+                level = loggerLevel
+            }
         }
     }

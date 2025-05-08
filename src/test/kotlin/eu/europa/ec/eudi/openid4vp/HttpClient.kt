a9199b9dfeeefc9b6c7e8240e407d5469fe18f0e
Anastasios Tsantoulis <anastasios.tsantoulis@netcompany.com>
2025-05-08 11:42:36 +0300

    Introduce http client logging to tests (#359)

---
+++ src/test/kotlin/eu/europa/ec/eudi/openid4vp/HttpClient.kt
@@ -0,0 +1,85 @@
+/*
+ * Copyright (c) 2023 European Commission
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package eu.europa.ec.eudi.openid4vp
+
+import io.ktor.client.*
+import io.ktor.client.engine.*
+import io.ktor.client.engine.okhttp.*
+import io.ktor.client.plugins.contentnegotiation.*
+import io.ktor.client.plugins.logging.*
+import io.ktor.serialization.kotlinx.json.*
+import java.security.SecureRandom
+import java.security.cert.X509Certificate
+import javax.net.ssl.HostnameVerifier
+import javax.net.ssl.SSLContext
+import javax.net.ssl.TrustManager
+import javax.net.ssl.X509TrustManager
+
+fun createHttpClient(
+    httpEngine: HttpClientEngine,
+    enableLogging: Boolean = false,
+): HttpClient =
+    HttpClient(httpEngine) {
+        if (enableLogging) {
+            install(Logging) {
+                logger = Logger.DEFAULT
+                level = LogLevel.ALL
+            }
+        }
+    }
+
+fun createHttpClient(enableLogging: Boolean = false): HttpClient =
+    HttpClient(OkHttp) {
+        engine {
+            config {
+                sslSocketFactory(
+                    sslSocketFactory = SslSettings.sslContext().socketFactory,
+                    trustManager = SslSettings.trustManager(),
+                )
+                hostnameVerifier(SslSettings.hostNameVerifier())
+            }
+        }
+        if (enableLogging) {
+            install(Logging) {
+                logger = Logger.DEFAULT
+                level = LogLevel.ALL
+            }
+        }
+        install(ContentNegotiation) { json() }
+
+        expectSuccess = true
+    }
+
+private object SslSettings {
+
+    fun sslContext(): SSLContext {
+        val sslContext = SSLContext.getInstance("TLS")
+        sslContext.init(null, arrayOf(trustManager()), SecureRandom())
+        return sslContext
+    }
+
+    fun hostNameVerifier(): HostnameVerifier = TrustAllHosts
+    fun trustManager(): X509TrustManager = TrustAllCerts as X509TrustManager
+
+    private var TrustAllCerts: TrustManager = object : X509TrustManager {
+        override fun checkClientTrusted(chain: Array<X509Certificate>, authType: String) {}
+        override fun checkServerTrusted(chain: Array<X509Certificate>, authType: String) {}
+        override fun getAcceptedIssuers(): Array<X509Certificate> {
+            return arrayOf()
+        }
+    }
+    private val TrustAllHosts: HostnameVerifier = HostnameVerifier { _, _ -> true }
+}
